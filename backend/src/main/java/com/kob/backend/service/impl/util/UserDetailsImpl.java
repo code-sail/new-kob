00fb3378118bb08010ed2baa36b011e508db87fc
Alvin <1911294076>
2024-10-10 14:44:33 +0800

    配置了mysql和登录注册模块 4.1上

---
+++ backend/src/main/java/com/kob/backend/service/impl/util/UserDetailsImpl.java
@@ -0,0 +1,54 @@
+package com.kob.backend.service.impl.util;
+
+import com.kob.backend.pojo.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {  //该账号是不是没有被锁定
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() { //是否过期
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() { // 该用户是不是被启用了
+        return true;
+    }
+}
--- backend/src/test/java/com/kob/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/kob/backend/BackendApplicationTests.java
@@ -2,12 +2,22 @@ package com.kob.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("pjmf"));
+        System.out.println(passwordEncoder.encode("pjmf"));
+
+        System.out.println(passwordEncoder.matches("pjmf", "$2a$10$zlQJSLGNhsx6TsIL1vEpcOe.XUdhspqQwvL0E44kb.ieA6g09AceC"));
+
+
+
     }
 
 }
